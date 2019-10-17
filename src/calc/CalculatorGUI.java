82380b8861411073f9e68956acd12376cd5057a2
jaykybert <51761831+jaykybert@users.noreply.github.com>
2019-10-17 19:59:59 +0100

    Added keyboard support for clear, rounded output.

--- src/calc/CalculatorGUI.java
+++ src/calc/CalculatorGUI.java
@@ -1,6 +1,7 @@
 package calc;
 
 import java.io.File;
+import java.text.DecimalFormat;
 import javafx.application.*;
 import javafx.scene.*;
 import javafx.stage.*;
@@ -14,12 +15,12 @@ public class CalculatorGUI extends Application {
 	StringBuilder numberString; // Concatenate the entered numbers.
 	StringBuilder outputText;	// Entire equation displayed on label.
 	String operation = ""; // One of: + - / *
-	
+
 	boolean equalsPressed = false;
-	
+
 	Double num1 = null, num2 = null, sum = 0.0;
-	
-	// Combination Keys (shift).
+
+	// Keyboard input for keys that require shift-press.
 	KeyCombination keyPlus = new KeyCodeCombination(KeyCode.EQUALS, KeyCodeCombination.SHIFT_DOWN);
 	KeyCombination keyMult = new KeyCodeCombination(KeyCode.DIGIT8, KeyCodeCombination.SHIFT_DOWN);
 
@@ -29,7 +30,7 @@ public class CalculatorGUI extends Application {
 		launch(args);
 	}
 
-	
+
 	private double getNumber() {
 		// Exception occurs if operation button is pressed when no numbers have been entered.
 		try {
@@ -39,8 +40,8 @@ public class CalculatorGUI extends Application {
 			return 0.0;
 		}
 	}
-	
-	
+
+
 	public void start(Stage stage) {
 		stage.setTitle("Calculator");
 		stage.setMinHeight(400);
@@ -49,17 +50,17 @@ public class CalculatorGUI extends Application {
 		stage.setMaxWidth(300);
 
 		FlowPane root = new FlowPane(Orientation.VERTICAL);
-		
+
 		numberString = new StringBuilder("");
 		outputText = new StringBuilder("");
 
 		GridPane mainPad = new GridPane();
-		
+
 		mainPad.setHgap(2);
 		mainPad.setVgap(2);
 		mainPad.setPadding(new Insets(10, 10, 10, 10));
 		mainPad.setAlignment(Pos.CENTER);
-		
+
 		output = new Label("");
 		output.setAlignment(Pos.CENTER);
 
@@ -87,31 +88,25 @@ public class CalculatorGUI extends Application {
 		mainPad.add(b2, 2, 0);
 		mainPad.add(b3, 3, 0);
 		mainPad.add(bA, 4, 0);
-		
 		mainPad.add(b4, 1, 1);
 		mainPad.add(b5, 2, 1);
 		mainPad.add(b6, 3, 1);
 		mainPad.add(bS, 4, 1);
-		
 		mainPad.add(b7, 1, 2);
 		mainPad.add(b8, 2, 2);
 		mainPad.add(b9, 3, 2);
 		mainPad.add(bD, 4, 2);
-		
 		mainPad.add(bC, 1, 3);
 		mainPad.add(b0, 2, 3);
 		mainPad.add(bE, 3, 3);
 		mainPad.add(bM, 4, 3);
-		
-		Scene scene = new Scene(root, 300, 400);
-		
-		root.getChildren().addAll(output, mainPad);
 
+		Scene scene = new Scene(root, 300, 400);
+		root.getChildren().addAll(output, mainPad);
 		scene.getStylesheets().clear();
 		scene.getStylesheets().add("file:///" + cssFile.getAbsolutePath().replace("\\", "/"));
 		stage.setScene(scene);
 		stage.show();
-
 
 		// Event Handling
 		b0.setOnAction((actionEvent) -> {
@@ -207,34 +202,35 @@ public class CalculatorGUI extends Application {
 				break;
 			case "*":
 				sum = num1 * num2;
-			}			
+			}
+			sum = Double.parseDouble(new DecimalFormat("#.###").format(sum));
 			outputText.replace(0, outputText.length(), " = " + sum);
 			output.setText(outputText.toString());
-			
+
 			num1 = sum; // The first number is now the sum.
 			num2 = 0.0;
 		});
-		
+
 		bA.setOnAction((actionEvent) -> {
 			operation = "+";
 			if(equalsPressed) // First number is now the result of the last equation.
 				num2 = getNumber();
 			else
 				num1 = getNumber();
-			
+
 			outputText.append("+");
 			// Reset the numberString after getting the first number.
 			numberString.setLength(0);	
 			output.setText(outputText.toString());		
 		});
-		
+
 		bS.setOnAction((actionEvent) -> {
 			operation = "-";
 			if(equalsPressed)
 				num2 = getNumber();
 			else
 				num1 = getNumber();
-			
+
 			outputText.append("-");
 			numberString.setLength(0);
 			output.setText(outputText.toString());	
@@ -246,7 +242,7 @@ public class CalculatorGUI extends Application {
 				num2 = getNumber();
 			else
 				num1 = getNumber();
-			
+
 			outputText.append("/");
 			numberString.setLength(0);
 			output.setText(outputText.toString());
@@ -258,46 +254,43 @@ public class CalculatorGUI extends Application {
 				num2 = getNumber();
 			else
 				num1 = getNumber();
-			
+
 			outputText.append("*");
 			numberString.setLength(0);
 			output.setText(outputText.toString());
 		});
-		
+
 		bP.setOnAction((actionEvent) -> {
 			outputText.append(".");
 			numberString.append(".");
 			output.setText(outputText.toString());
 		});
-		
+
 		// Keyboard Support
 		scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
-			if(key.getCode() == KeyCode.DIGIT0) b0.fire();
-			else if(key.getCode() == KeyCode.NUMPAD0) b0.fire();
-			else if(key.getCode() == KeyCode.DIGIT1) b1.fire();
-			else if(key.getCode() == KeyCode.NUMPAD1) b1.fire();
-			else if(key.getCode() == KeyCode.DIGIT2) b2.fire();
-			else if(key.getCode() == KeyCode.NUMPAD2) b2.fire();
-			else if(key.getCode() == KeyCode.DIGIT3) b3.fire();
-			else if(key.getCode() == KeyCode.NUMPAD3) b3.fire();
-			else if(key.getCode() == KeyCode.DIGIT4) b4.fire();
-			else if(key.getCode() == KeyCode.NUMPAD4) b4.fire();
-			else if(key.getCode() == KeyCode.DIGIT5) b5.fire();
-			else if(key.getCode() == KeyCode.NUMPAD5) b5.fire();	
-			else if(key.getCode() == KeyCode.DIGIT6) b6.fire();
-			else if(key.getCode() == KeyCode.NUMPAD6) b6.fire();		
-			else if(key.getCode() == KeyCode.DIGIT7) b7.fire();
-			else if(key.getCode() == KeyCode.NUMPAD7) b7.fire();
+
+			if(key.getCode() == KeyCode.DIGIT0 | key.getCode() == KeyCode.NUMPAD0) b0.fire();
+			else if(key.getCode() == KeyCode.DIGIT1 | key.getCode() == KeyCode.NUMPAD1) b1.fire();
+			else if(key.getCode() == KeyCode.DIGIT2 | key.getCode() == KeyCode.NUMPAD2) b2.fire();
+			else if(key.getCode() == KeyCode.DIGIT3 | key.getCode() == KeyCode.NUMPAD3) b3.fire();
+			else if(key.getCode() == KeyCode.DIGIT4 | key.getCode() == KeyCode.NUMPAD4) b4.fire();
+			else if(key.getCode() == KeyCode.DIGIT5 | key.getCode() == KeyCode.NUMPAD5) b5.fire();
+			else if(key.getCode() == KeyCode.DIGIT6 | key.getCode() == KeyCode.NUMPAD6) b6.fire();		
+			else if(key.getCode() == KeyCode.DIGIT7 | key.getCode() == KeyCode.NUMPAD7) b7.fire();
 			else if(keyMult.match(key)) bM.fire(); // Must come before b8, otherwise b8 fires.
-			else if(key.getCode() == KeyCode.DIGIT8) b8.fire();
-			else if(key.getCode() == KeyCode.NUMPAD8) b8.fire();
-			else if(key.getCode() == KeyCode.DIGIT9) b9.fire();
-			else if(key.getCode() == KeyCode.NUMPAD9) b9.fire();
+			else if(key.getCode() == KeyCode.DIGIT8 | key.getCode() == KeyCode.NUMPAD8)b8.fire();
+			else if(key.getCode() == KeyCode.DIGIT9 | key.getCode() == KeyCode.NUMPAD9) b9.fire();
 			else if(keyPlus.match(key)) bA.fire();
 			else if(key.getCode() == KeyCode.MINUS) bS.fire();
 			else if(key.getCode() == KeyCode.SLASH) bD.fire();
 			else if(key.getCode() == KeyCode.EQUALS) bE.fire();
 			else if(key.getCode() == KeyCode.DECIMAL) bP.fire();
+			else if(key.getCode() == KeyCode.DELETE | key.getCode() == KeyCode.BACK_SPACE) {
+				numberString.setLength(0);
+				num1 = num2 = 0.0;
+				outputText.setLength(0); 
+				output.setText(outputText.toString());
+			}
 		});
 	}
 }
